b18e2174eab36c024f1079ba2538af4b4e5105fd
kellydunn <defaultstring@gmail.com>
2012-04-12 21:03:15 -0700

    [src] Moving manager to module

--- src/amp.js
+++ src/amp.js
@@ -1,5 +1,4 @@
 (function (global) {
-  // Shamelessly following Backbone's lead
   var root = this;
   var Amp = null;
   Amp = (typeof exports !== 'undefined')? exports : root.Amp = {};
@@ -7,10 +6,6 @@
 
   // Histogram visualization based off of:
   // http://www.storiesinflight.com/jsfft/visualizer/index.html
-
-  var bufferSize = 2048;
-  var signal = new Float32Array(bufferSize);
-
   var modules = {};
 
   Amp.define = function(module, dependencies, fn) {
@@ -29,101 +24,5 @@
   if (typeof define === 'undefined') {
     global.define = Amp.define;
   }
-
-  Amp.Manager = {
-    ZOOM : 100,
-    range : 32,
-    timeout : 10,
-    jsProcessor : 0,
-    source : 0,
-
-    init_page: function() {
-      this.api = null;
-      this.context = null;
-      this.currentvalue = new Array();
-      this.initAudio();
-      if(this.api != null && this.api == "webkit") {
-        this.fft = new FFT(bufferSize, 44100);
-      }
-    },
-
-    // Mozilla API needs to wait for a different callback to properly set up FFT
-    init_moz_page: function() {
-      var framebufferlength = Amp.Manager.context.mozFrameBufferLength;
-      var channels = Amp.Manager.context.mozChannels;
-      var samplerate = Amp.Manager.context.mozSampleRate;
-      Amp.Manager.fft = new FFT(framebufferlength, samplerate);
-    },
-
-    loadSample: function(url) {
-      var request = Amp.Request.initialize({url: url});
-      request.send();
-    },
-
-    stop: function() {
-      if(this.source != 0) {
-        this.stopped = true;
-        this.source.noteOff(0);
-      }
-    },
-
-    initAudio: function() {
-      // TODO Debug for firefox
-      if(typeof webkitAudioContext == "undefined") {
-        this.api = "mozilla";
-        Amp.Manager.context = new Audio();
-        Amp.Manager.context.src = "/images/milkshake.ogg";
-        Amp.Manager.context.addEventListener('MozAudioAvailable', this.audioAvailable, false);
-        Amp.Manager.context.addEventListener('loadedmetadata', this.init_moz_page, false);
-        Amp.Manager.context.play();
-        Amp.Visualizer.animate();
-      } else {
-        this.api = "webkit";
-        this.context = new webkitAudioContext();
-        this.context.sampleRate = 44100;
-        Amp.Manager.source = this.context.createBufferSource();
-
-        this.jsProcessor = this.context.createJavaScriptNode(4096, 1, 2);
-        this.jsProcessor.onaudioprocess = this.audioAvailable.bind(this);
-
-        Amp.Manager.source.connect(this.jsProcessor);
-        this.jsProcessor.connect(this.context.destination);
-        this.loadSample(this.url);
-      }
-    },
-
-    // TODO Make seperate classes for seperate APIs
-    audioAvailable : function(event) {
-      if(this.api != null && this.api == "webkit") {
-        var inputArrayL = event.inputBuffer.getChannelData(0);
-        var inputArrayR = event.inputBuffer.getChannelData(1);
-        var outputArrayL = event.outputBuffer.getChannelData(0);
-        var outputArrayR = event.outputBuffer.getChannelData(1);
-
-        var n = inputArrayL.length;
-
-        for (var i = 0; i < n; i++) {
-          outputArrayL[i] = inputArrayL[i];
-          outputArrayR[i] = inputArrayR[i];
-          signal[i] = (inputArrayL[i] + inputArrayR[i]) / 2;
-        }
-      } else if (Amp.Manager.api != null && Amp.Manager.api == "mozilla") {
-        var framebuffer = event.frameBuffer;
-        var time = event.time;
-        var magnitude = null;
-        var framebufferlength = Amp.Manager.context.mozFrameBufferLength;
-
-        for(var i = 0, fbl = framebufferlength/2; i < fbl; i++) {
-          signal[i] = (framebuffer[2*i] + framebuffer[2*i+1]) / 2;
-        }
-      }
-
-      Amp.Manager.fft.forward(signal);
-
-      for ( var i = 0; i < bufferSize/Amp.Manager.range; i++ ) {
-        Amp.Manager.currentvalue[i] = (Amp.Manager.fft.spectrum[i] * Amp.Manager.ZOOM);
-      }
-    }
-  };
 
 })(typeof window === 'undefined' ? this : window);
--- src/amp/manager.js
+++ src/amp/manager.js
@@ -1,36 +1,3 @@
-(function (global) {
-  // Shamelessly following Backbone's lead
-  var root = this;
-  var Amp = null;
-  Amp = (typeof exports !== 'undefined')? exports : root.Amp = {};
-  var $ = root.jQuery;
-
-  // Histogram visualization based off of:
-  // http://www.storiesinflight.com/jsfft/visualizer/index.html
-
-  var bufferSize = 2048;
-  var signal = new Float32Array(bufferSize);
-
-  var modules = {};
-
-  Amp.define = function(module, dependencies, fn) {
-    if (typeof define === 'function' && define.amd) {
-      define(module, dependencies, fn);
-    } else {
-      if (dependencies && dependencies.length) {
-        for (var i = 0; i < dependencies.length; i++) {
-          dependencies[i] = modules[dependencies[i]];
-        }
-      }
-      modules[module] = fn.apply(this, dependencies || []);
-    }
-  };
-
-  if (typeof define === 'undefined') {
-    global.define = Amp.define;
-  }
-
-})(typeof window === 'undefined' ? this : window);
 define("Amp.Manager", ["Amp"], function(){
   Amp.Manager = {
     ZOOM : 100,
@@ -38,12 +5,14 @@ define("Amp.Manager", ["Amp"], function(){
     timeout : 10,
     jsProcessor : 0,
     source : 0,
+    bufferSize : 2048;
 
     init_page: function() {
       this.api = null;
       this.context = null;
       this.currentvalue = new Array();
       this.initAudio();
+      this.signal = new Float32Array(bufferSize);
       if(this.api != null && this.api == "webkit") {
         this.fft = new FFT(bufferSize, 44100);
       }
@@ -70,8 +39,7 @@ define("Amp.Manager", ["Amp"], function(){
     },
 
     initAudio: function() {
-      // TODO Debug for firefox
-      if(typeof webkitAudioContext == "undefined") {
+       if(typeof webkitAudioContext == "undefined") {
         this.api = "mozilla";
         Amp.Manager.context = new Audio();
         Amp.Manager.context.src = "/images/milkshake.ogg";
@@ -107,7 +75,7 @@ define("Amp.Manager", ["Amp"], function(){
         for (var i = 0; i < n; i++) {
           outputArrayL[i] = inputArrayL[i];
           outputArrayR[i] = inputArrayR[i];
-          signal[i] = (inputArrayL[i] + inputArrayR[i]) / 2;
+          this.signal[i] = (inputArrayL[i] + inputArrayR[i]) / 2;
         }
       } else if (Amp.Manager.api != null && Amp.Manager.api == "mozilla") {
         var framebuffer = event.frameBuffer;
@@ -116,7 +84,7 @@ define("Amp.Manager", ["Amp"], function(){
         var framebufferlength = Amp.Manager.context.mozFrameBufferLength;
 
         for(var i = 0, fbl = framebufferlength/2; i < fbl; i++) {
-          signal[i] = (framebuffer[2*i] + framebuffer[2*i+1]) / 2;
+          Amp.Manager.signal[i] = (framebuffer[2*i] + framebuffer[2*i+1]) / 2;
         }
       }
 
@@ -124,54 +92,6 @@ define("Amp.Manager", ["Amp"], function(){
 
       for ( var i = 0; i < bufferSize/Amp.Manager.range; i++ ) {
         Amp.Manager.currentvalue[i] = (Amp.Manager.fft.spectrum[i] * Amp.Manager.ZOOM);
-      }
-    }
-  };
-});
-define("Amp.Request", ["Amp"], function(){
-  Amp.Request = {
-    type: "GET",
-    initialize: function(options, f) {
-      if(options == null) {
-        options = [];
-        options["url"] = "";
-      }
-
-      this.self = new XMLHttpRequest();
-      this.self.open(this.type, options["url"], true);
-      this.self.responseType = "arraybuffer";
-
-      this.self.onload = (f != null) ? f : function() {
-        Amp.Manager.source.buffer = Amp.Manager.context.createBuffer(this.self.response, true);
-        Amp.Manager.source.loop = false;
-        Amp.Manager.source.noteOn(0);
-        Amp.Visualizer.animate();
-      }.bind(this);
-
-      return this.self;
-    }
-  };
-});
-define("Amp.Visualizer", ["Amp"], function(){
-  Amp.Visualizer = {
-    canvas : document.getElementById('fft'),
-    color : '#EEEEEE',
-    bar_width : 50,
-    bar_gap : 50,
-
-    animate: function() {
-      this.bar_height = this.canvas.height;
-      this.ctx = this.canvas.getContext('2d');
-      this.ctx.clearRect(0,0, this.canvas.width, this.canvas.height);
-      this.ctx.fillStyle = this.color;
-      for (var i=0; i<Amp.Manager.currentvalue.length; i++) {
-        this.ctx.fillRect(i * this.bar_gap, this.bar_height, this.bar_width, -Amp.Manager.currentvalue[i]*3);
-      }
-
-      t = setTimeout('Amp.Visualizer.animate()', Amp.Manager.timeout);
-      if(this.stopped) {
-        this.ctx.clearRect(0,0, this.canvas.width, this.canvas.height);
-        clearTimeout(t);
       }
     }
   };
